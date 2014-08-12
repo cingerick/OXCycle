6117bc29204e5f5f1d9ef325ff7d7fed57e9cbba
pensa1 <cingerick@pensanyc.com>
2014-08-12 14:22:15 -0400

    cleaning up

--- GUI/js/main.js
+++ GUI/js/main.js
@@ -9,25 +9,7 @@ var serialFlashed=false;
 var tinyGHomed=false;
 var serialQueue=[];
 
-var jogFeedFwd=false;
-var yAt0=false;
-var xAt0=false;
-var feedSent=0;
-var recievedX=0;
 
-var polyFormulaPos= [32.007904, 0.106508255, -3.3214688E-4, 5.4453267E-6];
-var polyFormulaNeg=[20.098206, -0.113582134, 5.997345E-4, -6.7329092E-6];
-var defaultFormulaPos= [32.007904, 0.106508255, -3.3214688E-4, 5.4453267E-6];
-var defaultFormulaNeg= [20.098206, -0.113582134, 5.997345E-4, -6.7329092E-6];
-
-var homeOffsetPos=30;
-var homeOffsetNeg=homeOffsetPos-8;
-
-var lastangle=0;
-
-var XFeedRate=450;
-var XG0Rate=450;
-var YG0Rate=40;
 
 /* Interprets an ArrayBuffer as UTF-8 encoded string data. */
 var ab2str = function(buf) {
@@ -125,27 +107,13 @@ function onOpen(openInfo) {
   //chrome.serial.read(connectionId, 1, onRead);
 };
 
-function setPosition(position) {
-  //var buffer = new ArrayBuffer(1);
-  //var uint8View = new Uint8Array(buffer);
-  //uint8View[0] = position;
-  console.log(position);
-  TinyG.send(position);
-};
-
-
 function useInput(st) {
 try
 {
    var json = JSON.parse(st);
    var r = json.r!=undefined  ? json.r  : null;
-   var sr= json.sr!=undefined ? json.sr : null;
    var er= json.er!=undefined ? json.er : null;
    var id= json.er!=undefined ? json.er : null;
-	if (sr!=null){
-	    var check1 = sr.posy!=undefined ? sr.posy : -999 ;
-        var check2 = sr.posx!=undefined ? sr.posx : -999 ;
-	}
 	else if (r!=null){
           var check1 = r.posy!=undefined ? r.posy : -999 ;
           var check2 = r.posx!=undefined ? r.posx : -999 ;
@@ -159,80 +127,17 @@ try
 	 else if (id!=null){
             tinyGFlashReady=true;
       }
-	  
-      
-      if (check3<=0){
-        check3= json.f!=undefined ? json.f.length : -1;
-      }
-	  
-	  
-	  if (check1 > -999 && !jogFeedFwd) {
-        if (check1==0){
-          yAt0=true;
-          println("Y at Zero");
-          feedSent=-999;
-        }
-        else if (check1==feedSent){
-          yAt0=true;
-          TinyGSafeWrite ("g28.3 y0" +'\n');
-        }
-        tinyGCommandReady=true;
-      }
-	  if (check2!=-999) {
-        recievedX=check2;
-        if (check2==0){
-          xAt0=true;
-          println("X at Zero");
-        }
-        else{
-          xAt0=false;
-        }
-        tinyGCommandReady=true;
-        //println ("at O");
-      }
+
       if (check3 > 0) {
         tinyGFlashReady=true;
-      }
-	  if (check4 != null) {
-        tinyGFlashReady=true;
-        setSerialNumber(check4);
-      }
-      if (check5>=0){
-        tinyGFlashReady=false;
-        sendPaused=true;
-        if (check5==0){
-          limitTripped=1;
-        }
-        else{
-          limitTripped=-1;
-        }
-        popUpWarning="limit_switch";
-        displayCase="warning";
-        
-      }
-      if (check7 == 1) {
-        homeResponse=true;
-        tinyGHomed=true;  
-        //yAt0=true;
-        //xAt0=true;
-        tinyGFlashReady=true;
-        println ("Homed Sweet Homed");
-      }
-	
-	
-	
-	
-	
-	
+      }	
 }
 catch(e)
 {
-println(e);
+	println(e);
    println('invalid json');
 }
 };
-
-
 
 function setStatus(status) {
   document.getElementById('status').innerText = status;
@@ -255,7 +160,6 @@ function testSerial(ports){
 		println(err);
 	}
 
-	  
   });
 
 }
